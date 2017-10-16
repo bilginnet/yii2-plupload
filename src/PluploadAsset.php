95c4a6b27c0e6b6c90835bde79156c624ee9d02f
Ercan Bilgin <bilginnet@hotmail.com>
2017-10-16 16:15:20 +0300

    Update PluploadAsset.php

--- src/PluploadAsset.php
+++ src/PluploadAsset.php
@@ -8,7 +8,7 @@ use yii\web\View;
 
 class PluploadAsset extends AssetBundle
 {
-    public $sourcePath = '@bilginnet/plupload/assets';
+    public $sourcePath = __DIR__ '/assets';
     public $jsOptions = ['position' => View::POS_END];
 
     public $js = [
